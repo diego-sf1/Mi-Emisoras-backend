e979e51092de877059014182e72827b766b2fd76
olveredgar <oled2002@gmail.com>
2021-11-08 14:49:12 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+//Intanciamos todo los que vamos a usar
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+const fs = require('fs')
+
+//iniciamos la aplicación
+app = express()
+
+//configuramos el motor de renderizado EJS
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+//Configuramos los endpoints para los archivos de acceso general
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/dev',express.static(path.join(__dirname,'node_modules/bootstrap/dist')))
+app.use('/ckeditor4',express.static(path.join(__dirname, 'node_modules/ckeditor4')));
+
+//usamos el middleware de express para sesiones, para que registre las peticiones al servidor
+app.use(session({
+	secret : '1234',
+	resave:false,
+	saveUninitialized:true,
+	cookie:{
+		sameSite:true
+	}
+}))
+
+//colocamos el enrutador
+require('./route')
+
+//colocamos una respuesta de error
+app.use((req,res)=>{
+  res.render('404')
+})
+
+
+app.listen(3000,()=>{
+  console.log('Servidor Iniciado en el puerto 3000')
+})
