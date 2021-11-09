db6bde052034954e70d04a89e627e9b849c4ccea
olveredgar <oled2002@gmail.com>
2021-11-09 12:23:15 -0500

    Update index.js

--- route/api/index.js
+++ route/api/index.js
@@ -4,7 +4,18 @@ const langStation = require('../../data/station_lang.json')
 
 apiRest.get('/listAll',(req,res)=>{
   let stations = []
-  
+  for(let gen in genStation){
+    for(let station in genStation[gen].stations){
+      let stationTemp=genStation[gen].stations[station]
+      stationTemp.id = station
+      stationTemp.gen = gen
+      stationTemp.genName = genStation[gen].name
+      stationTemp.langName = langStation[genStation[gen].stations[station].lang].name
+      stations.push(stationTemp)
+      
+    }
+  }
+  res.send(stations)
 })
 
 
