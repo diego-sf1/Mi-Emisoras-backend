76f4b61666ce1ca218cfb7ada0da93e727bc470e
olveredgar <oled2002@gmail.com>
2021-11-09 11:31:25 -0500

    Update index.js

--- route/api/index.js
+++ route/api/index.js
@@ -2,7 +2,7 @@ const apiRest = require('express').Router()
 const genStation = require('../../data/station_genre.json')
 const langStation = require('../../data/station_lang.json')
 
-apiRest.get('/',(req,res)=>{
+apiRest.get('/listAll',(req,res)=>{
   let stations = []
   
 })
